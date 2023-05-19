29b45f740b87d567babf02296530b10163dbc7ee
fateme.atsh <fateme.atsh@gmail.com>
2023-05-19 10:38:48 +0430

    fix delete item functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //  SELECTED ELEMENTS
 let filter = document.querySelector('input.filter');
-let tasksList = document.querySelector('table.list');
+let tasksList = document.querySelector('ul.list');
 let noTask = document.querySelector('p.no-task');
 let takForm = document.querySelector('form#task-form');
 let newTaskInput = document.getElementById('new-task-input');
@@ -16,6 +16,12 @@ function loadEventListeners() {
 
     // FILTER TASKS
     filter.addEventListener('keyup', filterTasks);
+
+    // REMOVE TASK ITEM
+    tasksList.addEventListener('click', onClickDeleteItem);
+
+    // EDIT TASK ITEM
+    tasksList.addEventListener('click', onClickEditItem);
 };
 
 // GET ALL TASKS STORED IN LOCALSTORAGE
@@ -28,21 +34,32 @@ function getAllTasks() {
         noTask.style.display = 'none';
 
         tasksStoredArray.forEach(function (item, index) {
-            let task = document.createElement('tr');
+            let task = document.createElement('li');
             task.className = 'list-item';
-            let span = task.appendChild(document.createElement('span'));
-            span.appendChild(document.createTextNode(item));
+            task.appendChild(document.createTextNode(item));
 
             // CREATE ICON ELEMENT
+            let div = document.createElement('div');
+            task.appendChild(div);
             let deleteIcon = document.createElement('i');
-            deleteIcon.className = 'fa fa-trash-o';
-            task.appendChild(deleteIcon);
+            deleteIcon.className = 'fa fa-trash-o delete-item';
+            div.appendChild(deleteIcon);
+            let editIcon = document.createElement('i');
+            editIcon.className = 'fa fa-pen edit-item';
+            div.appendChild(editIcon);
             tasksList.appendChild(task);
-        })
+        });
 
     } else {
         noTask.style.display = 'block';
-    }
+    };
+};
+
+// FILTER TASKS FUNCTION
+function filterTasks(e) {
+    e.preventDefault();
+
+    console.log(filter.value);
 };
 
 // ADD NEW TASK FUNCTION
@@ -55,37 +72,70 @@ function onSubmitNewTask(event) {
         noTask.style.display = 'none';
 
         // CREATE LIST ELEMENT
-        let task = document.createElement('tr');
+        let task = document.createElement('li');
         task.className = 'list-item';
-        let span = task.appendChild(document.createElement('span'));
-        span.appendChild(document.createTextNode(newTaskInput.value));
+        task.appendChild(document.createTextNode(newTaskInput.value));
+
         // CREATE ICON ELEMENT
+        let div = document.createElement('div');
+        task.appendChild(div);
         let deleteIcon = document.createElement('i');
-        deleteIcon.className = 'fa fa-trash-o';
-        task.appendChild(deleteIcon);
+        deleteIcon.className = 'fa fa-trash-o delete-item';
+        div.appendChild(deleteIcon);
+        let editIcon = document.createElement('i');
+        editIcon.className = 'fa fa-pen edit-item';
+        div.appendChild(editIcon);
         tasksList.appendChild(task);
         tasksList.style.display = 'block';
 
         storeTaskInLocalStorage(newTaskInput.value);
         newTaskInput.value = '';
-    }
+    };
 };
 
-// FILTER TASKS FUNCTION
-function filterTasks() {
-    console.log(filter.value)
-}
-
-// STORE NEW TASK
+// STORE NEW TASK IN LOCALSTORAGE
 function storeTaskInLocalStorage(task) {
     let tasks;
     if (localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
+    };
     tasks.push(task);
     localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+};
 
-localStorage.clear()
+// DELETE SPESIFIC LIST ITEM
+function onClickDeleteItem(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains('delete-item')) {
+        e.target.parentElement.parentElement.remove();
+        if (tasksList.childNodes.length === 0) {
+            tasksList.style.display = 'none';
+            noTask.style.display = 'block';
+        };
+        deleteItemFromLocalStorage(e.target.parentElement.parentElement.textContent);
+    };
+};
+
+// DELETE SPESIFIC LIST ITEM FROM LOCALSTORAGE
+function deleteItemFromLocalStorage(taskItem) {
+    let localStorageArray = JSON.parse(localStorage.getItem('tasks'));
+    localStorageArray.forEach(function (item, index) {
+        taskItem === item && localStorageArray.splice(index, 1);
+        localStorage.setItem('tasks', JSON.stringify(localStorageArray));
+    });
+};
+
+// EDIT SPESIFIC LIST ITEM
+function onClickEditItem(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains('edit-item')) {
+        let edit = e.target.parentElement.parentElement;
+        console.log(edit.textContent);
+    };
+};
+
+// localStorage.clear()
